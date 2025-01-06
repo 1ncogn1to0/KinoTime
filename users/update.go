f4e6223a3f5482ad00814cae4e85d0def95a777e
1ncogn1to0 <khairulin13@gmail.com>
2025-01-06 13:39:31 +0500

    Initial commit

---
+++ users/update.go
@@ -0,0 +1,58 @@
+package users
+
+import (
+	"PracticeServer/db"
+	"PracticeServer/logging"
+	"PracticeServer/models"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"go.uber.org/zap"
+	"net/http"
+)
+
+func UpdateUsers(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	w.Header().Set("Content-Type", "application/json")
+	var requestData models.User
+	// Парсинг JSON-данных из тела запроса
+	if err := json.NewDecoder(r.Body).Decode(&requestData); err != nil {
+		response := Response{Status: "fail", Message: "Некорректное JSON-сообщение"}
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+	if requestData.Name == "" || requestData.Email == "" {
+		response := Response{Status: "fail", Message: "Поля имя или почта не должны быть пустыми!"}
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	var user models.User
+	tx := db.DB.First(&user, id)
+
+	if tx.Error != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := Response{Status: "fail", Message: "Failed to get user: " + tx.Error.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+	user.Name = requestData.Name
+	user.Email = requestData.Email
+
+	tx = db.DB.Updates(&user)
+	if tx.Error != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		logging.Logger.Error("Failed to update user:", zap.Error(tx.Error))
+		response := Response{Status: "fail", Message: "Failed to update user: " + tx.Error.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+	logging.Logger.Info("User updated successfully", zap.String("func", "UpdateUsers"))
+	response := Response{Status: "success", Message: "User updated successfully"}
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(response)
+	return
+}
