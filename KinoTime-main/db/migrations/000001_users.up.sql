eba44a7ed496d7cead59024e128681c8f34a0f15
1ncogn1to0 <161634035+1ncogn1to0@users.noreply.github.com>
2025-01-06 14:19:09 +0500

    Delete KinoTime-main directory

--- KinoTime-main/db/migrations/000001_users.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,        -- Уникальный идентификатор пользователя, автоматически увеличивается
-    name VARCHAR(255) NOT NULL,   -- Имя пользователя, текстовое поле, обязательное
-    email VARCHAR(255) NOT NULL,  -- Email пользователя, текстовое поле, обязательное
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Время создания записи, значение по умолчанию — текущее время
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Время последнего обновления
-);
